f129ef2cf55b67d5ed6f06a5becc0459f281bd72
Evgeny Khoroshavin <themplarer@gmail.com>
2023-11-02 01:20:28 +0500

    Больше подробностей (часть 1)

--- src/components/films/film-card.tsx
+++ src/components/films/film-card.tsx
@@ -1,6 +1,8 @@
-import { useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { AppRoute } from '../../consts';
 import { Film } from '../../types/film';
+import PreviewPlayer from '../preview-player/preview-player';
+import { useState } from 'react';
 
 type FilmCardProps = {
   film: Film;
@@ -9,17 +11,34 @@ type FilmCardProps = {
 }
 
 function FilmCard({ film, onMouseEnter, onMouseLeave }: FilmCardProps): JSX.Element {
-  const navigate = useNavigate();
+  const width = 280;
+  const height = 175;
+
+  const [timer, setTimer] = useState<NodeJS.Timeout | undefined>(undefined);
+  const [isHovered, setIsHovered] = useState(false);
+
+  const handleMouseEnter = () => {
+    setTimer(setTimeout(() => setIsHovered(true), 1000));
+    onMouseEnter(film.id);
+  };
+
+  const handleMouseLeave = () => {
+    clearTimeout(timer);
+    setIsHovered(false);
+    onMouseLeave();
+  };
 
   return (
-    <article className="small-film-card catalog__films-card" onMouseEnter={() => onMouseEnter(film.id)} onMouseLeave={onMouseLeave} onClick={() => navigate(`/${AppRoute.Films}/${film.id}`)}>
+    <article className="small-film-card catalog__films-card" onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
       <div className="small-film-card__image">
-        <img src={film.posterImage} alt={film.name} width="280" height="175" />
+        {
+          isHovered
+            ? <PreviewPlayer videoLink={film.videoLink} poster={film.posterImage} width={width} height={height} />
+            : <img src={film.posterImage} width={width} height={height} />
+        }
       </div>
       <h3 className="small-film-card__title">
-        <a className='small-film-card__link'>
-          {film.name}
-        </a>
+        <Link className="small-film-card__link" to={`/${AppRoute.Films}/${film.id}`}>{film.name}</Link>
       </h3>
     </article>
   );
